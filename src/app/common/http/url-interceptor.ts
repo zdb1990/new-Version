419a0c4b723677cee3ab357be2ee8b37a9d82063
zhangyao <zhang_yao@uxsino.com>
2018-06-20 17:56:18 +0800

    修改请求端口

--- src/app/common/http/url-interceptor.ts
+++ src/app/common/http/url-interceptor.ts
@@ -6,12 +6,17 @@ export class InterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log(req);
         if (req.url) {
-            let url = req.url;
-            if (!url.startsWith('http')) {
-                url = url.startsWith('/') ? url : `/${url}`;
-                url = `http://localhost:8085${url}`;
-                console.log(url, req);
-                // req.url = url;
+            let Baseurl = req.url;
+            // console.log(url);
+            if (!Baseurl.startsWith('http')) {
+                Baseurl = Baseurl.startsWith('/') ? Baseurl : `/${Baseurl}`;
+                Baseurl = `http://localhost:8085${Baseurl}`;
+                console.log(Baseurl);
+                req['url'] = Baseurl;
+                // req.clone({
+                //     url: url
+                // });
+                console.log(req);
             }
         }
         return next.handle(req);
