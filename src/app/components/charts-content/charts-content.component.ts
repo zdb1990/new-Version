c219a21cb9f4b45c1b3a3b0658f449dd54038645
zhangyao <zhang_yao@uxsino.com>
2018-07-23 11:52:00 +0800

    修改图形

--- src/app/components/charts-content/charts-content.component.ts
+++ src/app/components/charts-content/charts-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'uxsino-charts-content',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./charts-content.component.scss']
 })
 export class ChartsContentComponent implements OnInit {
-
+  @Input() boxShow: Boolean;
   constructor() { }
 
   ngOnInit() {
