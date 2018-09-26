757b2145c0c4396d46dd39fd47cd9ed8f42b8348
zhangyao <zhangyao@parim.net>
2018-09-26 21:45:33 +0800

    修改导航

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,14 @@
-import { OpenItemDirective } from './../shared/open-item.directive';
+import { OpenChildDirective, OpenItemDirective } from './../shared/open-item.directive';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsRoutingModule, routedComponents } from './components-routing.module';
-
-
 @NgModule({
     imports: [
         SharedModule,
         ComponentsRoutingModule
     ],
     exports: [],
-    declarations: [...routedComponents, OpenItemDirective],
+    declarations: [...routedComponents, OpenChildDirective, OpenItemDirective],
     providers: [],
 })
 export class ComponentsModule { }
--- src/app/shared/open-item.directive.ts
+++ src/app/shared/open-item.directive.ts
@@ -14,22 +14,62 @@ export class OpenItemDirective {
   }
   @HostListener('click') onClick() {
     for (let i = 0; i < $('.nav-item').length; i++) {
-      $('.nav-item').eq(i).find('.nav-item-children').css('display', 'none');
+      // $('.nav-item').eq(i).find('.nav-item-children').css('display', 'none');
       if ($('.nav-item')[i].attr === 'directive') {
         $('.nav-item')[i]['indexed'] = i;
         if (this.el.nativeElement['indexed'] !== $('.nav-item')[i]['indexed']) {
-          $('.nav-item').eq(i).find('.nav-item-children').css('display', 'none');
+          $('.nav-item').eq(i).find('.nav-item-children').slideUp();
           $('.nav-item')[i]['flag'] = false;
         } else {
           console.log(1);
           this.el.nativeElement['flag'] = !this.el.nativeElement['flag'];
           if (this.el.nativeElement['flag']) {
-            $('.nav-item').eq(i).find('.nav-item-children').css('display', 'block');
+            $('.nav-item').eq(i).find('.nav-item-children').slideDown();
           } else {
-            $('.nav-item').eq(i).find('.nav-item-children').css('display', 'none');
+            $('.nav-item').eq(i).find('.nav-item-children').slideUp();
           }
         }
       }
     }
+  }
+}
+
+@Directive({
+  selector: '[openchild]'
+})
+export class OpenChildDirective {
+  constructor(private el: ElementRef, private open: OpenItemDirective) {
+    el.nativeElement['flag'] = false;
+    el.nativeElement['attr'] = 'directive';
+    el.nativeElement['indexed'] = '';
+  }
+  @HostListener('click', ['$event']) onclick(event: any) {
+
+    console.log(event);
+    event.stopPropagation();
+    let elment = this.open['el'].nativeElement['indexed'];
+    console.log(elment);
+    for (let i = 0; i < $('.nav-item').eq(elment).find('.children-item').length; i++) {
+      console.log($('.nav-item').eq(elment).find('.children-item')[i].attr);
+      if ($('.nav-item').eq(elment).find('.children-item')[i].attr === 'directive') {
+        $('.nav-item').eq(elment).find('.children-item')[i]['indexed'] = i;
+        console.log($('.nav-item').eq(elment).find('.children-item'));
+        console.log(this.el.nativeElement['indexed']);
+        if (this.el.nativeElement['indexed'] !== $('.nav-item').eq(elment).find('.children-item')[i]['indexed']) {
+          $('.nav-item').eq(elment).find('.children-item').eq(i).find('.nav-item-last').slideUp();
+          $('.nav-item').eq(elment).find('.children-item')[i]['flag'] = false;
+        } else {
+          console.log(1);
+          this.el.nativeElement['flag'] = !this.el.nativeElement['flag'];
+          if (this.el.nativeElement['flag']) {
+            $('.nav-item').eq(elment).find('.children-item').eq(i).find('.nav-item-last').slideDown();
+            // $('.nav-item').eq(i).find('.nav-item-children').slideDown();
+          } else {
+            $('.nav-item').eq(elment).find('.children-item').eq(i).find('.nav-item-last').slideUp();
+          }
+        }
+      }
+    }
+    console.log(this.el);
   }
 }
